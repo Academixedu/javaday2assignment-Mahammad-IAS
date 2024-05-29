f5f08abe98950992e24c1101c9160ea3e030fa6c
Mahammad-IAS <sharifsagarimahammad464@gmail.com>
2024-05-29 10:39:07 +0530

    first commit

--- src/main/java/com/example/StudentGradeEvaluation.java
+++ src/main/java/com/example/StudentGradeEvaluation.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class StudentGradeEvaluation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         // Prompt the user to enter their name
         System.out.print("Enter your name: ");
         String name = scanner.nextLine();
@@ -20,22 +19,22 @@ public class StudentGradeEvaluation {
         int score = scanner.nextInt();
         System.out.println("Score: "+score );
         // Determine the grade
-        String grade;
-        if (score>=90){
-            System.out.println("A");
-        }else if(score>=80){
-            System.out.println("B");
-        }else if(score>=70){
-            System.out.println("C");
-        }else if(score>=60){
-            System.out.println("D");
-        }else{
-            System.out.println("F");
-        }
+        String grade=scanner.nextLine();
+        System.out.println();
         // Print the student's details
         System.out.println("Name: "+ name);
         System.out.println("Age: "+ age);
         System.out.println("Score: "+ score);
-        System.out.println("Grade: A");  
+        if (score>=90){
+            System.out.println("Grade: A");
+        }else if(score>=80){
+            System.out.println("Grade: B");
+        }else if(score>=70){
+            System.out.println("Grade: C");
+        }else if(score>=60){
+            System.out.println("Grade: D");
+        }else{
+            System.out.println("F");
+        }
     }
 }
