bdb4523ce62a5d3e1885d5e4281c38f8a0cb8280
Mahammad-IAS <sharifsagarimahammad464@gmail.com>
2024-05-29 10:39:59 +0530

    first commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,29 @@
+package com.example;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+       
+        /* 
+        for (int i=0; i<=10; i++){
+            System.out.println("Mahammad"+ i);
+        }
+        int count =0;
+        while (count<=10) {
+            System.out.println("Mahammad"+count);
+            count++;
+        }
+        int doCount =0;
+        do {
+            System.out.println("Do while loop"+ doCount)
+        }
+        */
+        int i;
+        for (i=1; i<10; i++){
+            System.out.println(i);
+        }
+    }
+}
